448e44d196891cf04afd0f387a2dbde87dd48bd0
Wrath-y <805655528@qq.com>
2023-09-08 15:04:01 +0800

    feat: 添加cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,7 +10,7 @@ func CORS(c *core.Context) {
 	c.Header("Access-Control-Allow-Credentials", "true")
 
 	c.Header("Access-Control-Allow-Headers", "*")
-	c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
+	c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 
 	if c.Request.Method == http.MethodOptions {
 		c.AbortWithStatus(http.StatusNoContent)
